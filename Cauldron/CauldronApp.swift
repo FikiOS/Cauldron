aff993909d20f96078506fbe3fe691e4fe531b36
David Alarcon <developer@davidalarcon.dev>
2025-01-25 11:45:12 +0100

    Init commit.

---
+++ Cauldron/CauldronApp.swift
@@ -0,0 +1,17 @@
+//
+//  CauldronApp.swift
+//  Cauldron
+//
+//  Created by David Alarcon on 25/1/25.
+//
+
+import SwiftUI
+
+@main
+struct CauldronApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ CauldronTests/CauldronTests.swift
@@ -0,0 +1,17 @@
+//
+//  CauldronTests.swift
+//  CauldronTests
+//
+//  Created by David Alarcon on 25/1/25.
+//
+
+import Testing
+@testable import Cauldron
+
+struct CauldronTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
