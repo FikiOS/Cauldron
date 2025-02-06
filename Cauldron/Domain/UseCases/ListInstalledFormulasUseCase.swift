7f42e45ccbd14face992114c0aeb514d10299050
David Alarcon <developer@davidalarcon.dev>
2025-02-06 20:59:47 +0100

    Add execute command xpc service.

--- Cauldron/Domain/UseCases/ListInstalledFormulasUseCase.swift
+++ Cauldron/Domain/UseCases/ListInstalledFormulasUseCase.swift
@@ -12,8 +12,25 @@ struct ListInstalledFormulaesUseCase: ListInstalledFormulaesUseCaseProtocol {
   }
 
   func installedFormulaesAndCasks() async throws -> [Formulae] {
+    /*
+     let command = ListInstalledFormulasCommand()
+     let response: ListInstalledResponse = try await service.execute(command)
+
+     return response.formulae.map { Formulae(name: $0.name, fullName: $0.fullName, linkedKeg: $0.linkedKeg ?? "--") }
+     */
+    let connection = NSXPCConnection(serviceName: "dag.fikios.BrewCommandService")
+    connection.remoteObjectInterface = NSXPCInterface(with: BrewCommandServiceProtocol.self)
+    connection.resume()
+
+    guard let proxy = connection.remoteObjectProxy as? BrewCommandServiceProtocol else {
+      throw BrewError.brewNotFound
+    }
+
     let command = ListInstalledFormulasCommand()
-    let response: ListInstalledResponse = try await service.execute(command)
+
+    let data = try await proxy.runBrewCommand(path: "/opt/homebrew/bin/brew",
+                                              arguments: command.commandLine)
+    let response =  try JSONDecoder().decode(ListInstalledResponse.self, from: data)
 
     return response.formulae.map { Formulae(name: $0.name, fullName: $0.fullName, linkedKeg: $0.linkedKeg ?? "--") }
   }
